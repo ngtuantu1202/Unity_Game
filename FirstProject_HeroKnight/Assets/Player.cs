218f32f5185e2c89f695454e8258ae32c58c39c5
Nguyễn Tuấn Tú <nguyentuantu1202@gmail.com>
2025-03-17 14:17:20 +0700

    Create Project, Setup Player, Ground, Movement

---
+++ FirstProject_HeroKnight/Assets/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //private float movement = 0f;
+    private float movement;
+    public float moveSpeed = 6f;
+    private bool facingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        if (movement < 0f && facingRight)
+        {
+            transform.eulerAngles = new Vector3(0f, -180f, 0f);
+            facingRight = false;
+        }  
+        else if (movement > 0f && facingRight == false)
+        {
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            facingRight = true;
+        }    
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position += new Vector3(movement, 0f, 0f) * Time.fixedDeltaTime * moveSpeed;
+    }
+}
