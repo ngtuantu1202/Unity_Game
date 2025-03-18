2232585c9bd1b4c299bace289345bb9f98e985ed
Nguyễn Tuấn Tú <nguyentuantu1202@gmail.com>
2025-03-18 12:31:31 +0700

    Jump, Animation

--- FirstProject_HeroKnight/Assets/Player.cs
+++ FirstProject_HeroKnight/Assets/Player.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    //private float movement = 0f;
+    //di chuyen
     private float movement;
     public float moveSpeed = 6f;
+    //xoay huong
     private bool facingRight = true;
+    //nhay
+    public Rigidbody2D rb;
+    public float jumpHeight = 15f;
+    public bool isGround = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +23,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //di chuyen
         movement = Input.GetAxis("Horizontal");
         if (movement < 0f && facingRight)
         {
@@ -27,11 +34,30 @@ public class Player : MonoBehaviour
         {
             transform.eulerAngles = new Vector3(0f, 0f, 0f);
             facingRight = true;
-        }    
+        }
+        //nhay
+        if (Input.GetKey(KeyCode.Space) && isGround)
+        {
+            Jump();
+            isGround = false;
+        }
     }
 
     private void FixedUpdate()
     {
         transform.position += new Vector3(movement, 0f, 0f) * Time.fixedDeltaTime * moveSpeed;
+    }
+
+    void Jump()
+    {
+        rb.AddForce(new Vector2 (0f, jumpHeight), ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGround = true;
+        }    
     }
 }
